3ad4930b549675fb6ba2916282459db29f3e37e7
Andreas Nauleau <anauleau@gmail.com>
2015-12-09 11:04:05 -0800

    first commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name plaidProjectApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the plaidProjectApp
+ */
+angular.module('plaidProjectApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name plaidProjectApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the plaidProjectApp
+ */
+angular.module('plaidProjectApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
